3981118fa14565623b2db9462853dcfb9068a4bc
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2021-01-04 14:55:08 -0500

    fixed create/update routes

--- controllers/flora_controller.js
+++ controllers/flora_controller.js
@@ -22,12 +22,48 @@ flora.get('/new', (req, res) => {
 
 // NEW ITEM REDIRECT
 flora.post('/', (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-    // Flora.create(req.body, (error, newItem) => {
-    //   res.redirect('/flora');
-    // });
+  // console.log(req.body);
+  // res.send(req.body);
+  Flora.create(req.body, (error, newItem) => {
+      res.redirect('/flora');
+    });
 });
+
+// SEED
+flora.get('/seed', (req, res) => {
+  Flora.create(
+    [
+      {
+        name:'Tulip',
+        descr:'Known as a symbol of wealth and prosperity, the Dutch Tulip is world famous. Originally a product of the Ottoman Empire, it took the Dutch horticulturalist, Carolis Clusius, to pen a book on their wonder to push them into popularity. During a time known as \'Tulip Fever\', a single Tulip bulb could sell for as much as a traditional house in Amsterdam!',
+        nation: 'Holland',
+        img: 'https://cdn.shopify.com/s/files/1/1902/7917/products/Eternal_Spring_Mix_1_715x880_crop_center.JPG?v=1541177198',
+        price: '9',
+        qty: '12',
+      },
+      {
+        name:'Dahlia',
+        descr:'With an extensive history amongst the indigenous peoples as being highly useful medicinally as well as nutritionally, the Dahlia became the flower of Mexico in 1963. After Spainards discovered its usefulness in 1525 while conquesting through Mexico, it became a flower of intrique amongst European botanist and spread to France, Germany as well as Italy in the late 1700\'s.',
+        nation: 'Mexico',
+        img: 'https://www.floresdelcorazon.com/wp-content/uploads/2018/05/sin-pie-3-min.jpg',
+        price: '12',
+        qty: '4',
+      },
+      {
+        name:'Plum Blossom',
+        descr:'Symbolizing resilience, this flower became the national flower of the Republic of China in 1964. It grows on plum trees which have been known to grow for a very long time - some as long as 1\,600 years - hence it\'s symbolism.',
+        nation: 'China',
+        img: 'https://blog.russianflora.com/wp-content/uploads/2019/06/Depositphotos_41188461_s-2019.jpg',
+        price: '15',
+        qty: '8',
+      },
+    ],
+    (error, data) => {
+      res.redirect('/flora')
+    }
+  )
+});
+
 
 // SHOW
 flora.get('/:id', (req, res) => {
@@ -76,42 +112,6 @@ flora.post('/', (req, res) => {
         res.redirect('/flora');
     });
 });
-
-// SEED
-flora.get('/seed', (req, res) => {
-  Flora.create(
-    [
-      {
-        name:'Tulip',
-        descr:'Known as a symbol of wealth and prosperity, the Dutch Tulip is world famous. Originally a product of the Ottoman Empire, it took the Dutch horticulturalist, Carolis Clusius, to pen a book on their wonder to push them into popularity. During a time known as \'Tulip Fever\', a single Tulip bulb could sell for as much as a traditional house in Amsterdam!',
-        nation: 'Holland',
-        img: 'https://cdn.shopify.com/s/files/1/1902/7917/products/Eternal_Spring_Mix_1_715x880_crop_center.JPG?v=1541177198',
-        price: '9',
-        qty: '12',
-      },
-      {
-        name:'Dahlia',
-        descr:'With an extensive history amongst the indigenous peoples as being highly useful medicinally as well as nutritionally, the Dahlia became the flower of Mexico in 1963. After Spainards discovered its usefulness in 1525 while conquesting through Mexico, it became a flower of intrique amongst European botanist and spread to France, Germany as well as Italy in the late 1700\'s.',
-        nation: 'Mexico',
-        img: 'https://www.floresdelcorazon.com/wp-content/uploads/2018/05/sin-pie-3-min.jpg',
-        price: '12',
-        qty: '4',
-      },
-      {
-        name:'Plum Blossom',
-        descr:'Symbolizing resilience, this flower became the national flower of the Republic of China in 1964. It grows on plum trees which have been known to grow for a very long time - some as long as 1\,600 years - hence it\'s symbolism.',
-        nation: 'China',
-        img: 'https://blog.russianflora.com/wp-content/uploads/2019/06/Depositphotos_41188461_s-2019.jpg',
-        price: '15',
-        qty: '8',
-      },
-    ],
-    (error, data) => {
-      res.redirect('/flora')
-    }
-  )
-});
-
 
 // BUY
 flora.put("/buy/:id", (req, res) => {
